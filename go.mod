11899dbc76f17c3096ceb9d3c359e2581be4bd73
ohadApiiro <ohad@apiiro.com>
2022-11-30 12:08:49 +0200

    first

---
+++ go.mod
@@ -0,0 +1,6 @@
+module hello
+
+go 1.18
+require (
+	github.com/gobwas/glob v0.2.3
+)
---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+func log(msg string) {
+	//f, err := os.Create("/home/ohad/src/go/hello/dbg.txt")
+	//
+	//if err != nil {
+	//	return
+	//}
+	//
+	//defer f.Close()
+	//_, err2 := f.WriteString(msg + "\n")
+	//
+	//if err2 != nil {
+	//	return
+	//}
+}
+
+func main() {
+	log("abc")
+log("abc")
+}
